9c2137d83e61f6fdf28a3d50e8a35ddfb0013479
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2025-07-08 22:15:41 +0200

    Make front unit test codecove > 80%.

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,29 +1,49 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 
 import { AppComponent } from './app.component';
+import { SessionService } from './services/session.service';
 
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let sessionService: SessionService;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         MatToolbarModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    sessionService = TestBed.inject(SessionService);
+    router = TestBed.inject(Router);
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it ('is logged should return true', () => {
+    sessionService.isLogged = true;
+    expect(app.$isLogged()).toBeTruthy();
+  });
+
+  it ('is logout should navigate to /', () => {
+    let navSpy = jest.spyOn(router, 'navigate');
+    sessionService.isLogged = true;
+    app.logout();
+    expect(navSpy).toBeCalledWith([''])
   });
 });
